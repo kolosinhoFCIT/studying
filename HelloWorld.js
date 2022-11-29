a17092e5ea62594250e6e8a609e36a977d4764f5
kolosinho <example@gmail.com>
2022-11-29 22:55:35 +0200

    test commit

---
+++ HelloWorld.js
@@ -0,0 +1,9 @@
+const storeName = "ATB";
+let storeDesctiption = {
+    budget: 10000,
+    employees: ["Alex", "Billy", "Andy"],
+    products: {"pen": 100, "pencil": 50},
+    open: true
+};
+
+console.log(storeDesctiption.employees[0], storeDesctiption.products['pen'])
